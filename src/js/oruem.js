bf8c3c4c87d8bf9ab5747434c53182245f6c5794
jhyun-k <jhkim1238@naver.com>
2022-11-11 18:58:18 +0900

    info 안뜸

--- src/js/oruem.js
+++ src/js/oruem.js
@@ -143,7 +143,7 @@ window.initMap = function () {
 
               const info = document.querySelector('.info')
               const explain = `${가게이름} `
-              info.innerHTML = `${explain} :  ${주소} \n <a href="${url}">${url}</a>`
+              info.innerHTML = `${explain} : ${주소} \n <a href="${url}">${url}</a>`
               infowindow.setContent(explain);
               infowindow.open({
                 anchor: marker,
